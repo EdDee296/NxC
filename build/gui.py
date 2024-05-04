75af2225f6fbfb5f9c52677f9e3c48891eb2f6c4
EdDee296 <109675085+EdDee296@users.noreply.github.com>
2024-05-03 22:25:37 -0600

    finish first step

--- build/gui.py
+++ build/gui.py
@@ -1,23 +1,16 @@
-
-
 from pathlib import Path
-from tkinter import *
 from tkinter import filedialog
 import sys
 sys.path.append('C:/Users/ASUS/OneDrive/New folder/New folder')
 
 # Explicit imports to satisfy Flake8
-from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage, END
 from main import *
 
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\ASUS\Downloads\Tkinter-Designer-master\Tkinter-Designer-master\build\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\ASUS\OneDrive\New folder\New folder\build\assets\frame0")
 
-
-name = ''
-data_path = ''
-logs_path = ''
 
 def save():
     global name, data_path, logs_path
@@ -27,10 +20,9 @@ def save():
     print(name, data_path, logs_path)
 
 
-def openFile(window, type):
+def openFile(type):
     if type == "data":
         global data_path  # Use the global keyword to modify the global variable
-        pack = 0
         data_path = filedialog.askdirectory()
         data_path = data_path.replace('/', '\\')
         # Get a list of all entries (files and directories) in the folder
@@ -43,9 +35,8 @@ def openFile(window, type):
             for entry in entries:
                 full_path = os.path.join(data_path, entry)
                 if not os.path.isdir(full_path):
-                    label = Label(window, text=f"Make sure there are only different folders for different classes in your data directory. Please select again. ❌\n")
-                    label.pack()
-                    pack = 1
+                    entry_2.delete(0,END)
+                    entry_2.insert(0,"Invalid directory.❌")
                     break
                 else:
                     files = os.listdir(full_path)
@@ -55,34 +46,30 @@ def openFile(window, type):
                             subdirectories.append(file)
 
             if subdirectories:
-                if pack == 0:
-                    label_subdirs = Label(window, text=f"There exists subfolder(s) in your classes folder. Please select again. ❌\n {subdirectories}")
-                    label_subdirs.pack()
+                entry_2.delete(0)
+                entry_2.insert(0,"Invalid directory.❌")
             else:
-                if pack == 0:
-                    label_success = Label(window, text=f"Data Path Selected: {data_path} ✅")
-                    label_success.pack()
+                entry_2.delete(0,END)
+                entry_2.insert(0,data_path)
                     
 
         else:
-            label = Label(window, text=f"Empty directory. Please select a valid directory. ❌")
-            label.pack()
+            entry_2.delete(0,END)
+            entry_2.insert(0,"Invalid directory.❌")
     else:
         global logs_path
         logs_path = filedialog.askdirectory()
         logs_path = logs_path.replace('/', '\\')
         if os.path.isdir(logs_path):
-            label = Label(window, text=f"Logs Path Selected: {logs_path} ✅")
-            label.pack()
+            entry_3.delete(0,END)
+            entry_3.insert(0,logs_path)
         else:
-            label = Label(window, text=f"This is not a valid directory for logs. Please select a valid directory. ❌")
-            label.pack()
+            entry_3.delete(0,END)
+            entry_3.insert(0,"Invalid directory.❌")
 
 
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
-
-
 
 
 window = Tk()
@@ -110,13 +97,12 @@ canvas.create_rectangle(
     fill="#080707",
     outline="")
 
-canvas.create_text(
-    275.0,
-    0.0,
-    anchor="nw",
-    text="Nx-C",
-    fill="#FFFFFF",
-    font=("Italiana Regular", 48 * -1)
+image_image_1 = PhotoImage(
+    file=relative_to_assets("image_1.png"))
+image_1 = canvas.create_image(
+    335.0,
+    34.0,
+    image=image_image_1
 )
 
 entry_image_1 = PhotoImage(
@@ -132,7 +118,6 @@ entry_1 = Entry(
     fg="#000716",
     highlightthickness=0
 )
-
 entry_1.place(
     x=383.5,
     y=122.0,
@@ -140,13 +125,6 @@ entry_1.place(
     height=31.0
 )
 
-entry_image_2 = PhotoImage(
-    file=relative_to_assets("entry_2.png"))
-entry_bg_2 = canvas.create_image(
-    489.0,
-    312.5,
-    image=entry_image_2
-)
 entry_2 = Entry(
     bd=0,
     bg="#D9D9D9",
@@ -160,12 +138,12 @@ entry_2.place(
     height=31.0
 )
 
-entry_image_3 = PhotoImage(
-    file=relative_to_assets("entry_3.png"))
-entry_bg_3 = canvas.create_image(
+entry_image_2 = PhotoImage(
+    file=relative_to_assets("entry_2.png"))
+entry_bg_2 = canvas.create_image(
     489.0,
-    225.5,
-    image=entry_image_3
+    312.5,
+    image=entry_image_2
 )
 entry_3 = Entry(
     bd=0,
@@ -180,31 +158,37 @@ entry_3.place(
     height=31.0
 )
 
-canvas.create_text(
-    367.0,
-    99.0,
-    anchor="nw",
-    text="Model’s name ",
-    fill="#000000",
-    font=("Italiana Regular", 12 * -1)
+entry_image_3 = PhotoImage(
+    file=relative_to_assets("entry_3.png"))
+entry_bg_3 = canvas.create_image(
+    489.0,
+    225.5,
+    image=entry_image_3
 )
 
-canvas.create_text(
-    367.0,
-    182.0,
-    anchor="nw",
-    text="Data Path",
-    fill="#000000",
-    font=("Italiana Regular", 12 * -1)
+
+image_image_2 = PhotoImage(
+    file=relative_to_assets("image_2.png"))
+image_2 = canvas.create_image(
+    395,
+    277.0,
+    image=image_image_2
 )
 
-canvas.create_text(
-    367.0,
-    266.0,
-    anchor="nw",
-    text="Logs Path",
-    fill="#000000",
-    font=("Italiana Regular", 12 * -1)
+image_image_3 = PhotoImage(
+    file=relative_to_assets("image_3.png"))
+image_3 = canvas.create_image(
+    395,
+    193.0,
+    image=image_image_3
+)
+
+image_image_4 = PhotoImage(
+    file=relative_to_assets("image_4.png"))
+image_4 = canvas.create_image(
+    400,
+    110.0,
+    image=image_image_4
 )
 
 button_image_1 = PhotoImage(
@@ -213,14 +197,14 @@ button_1 = Button(
     image=button_image_1,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: openFile(window, "data"),
+    command=lambda: save(),
     relief="flat"
 )
 button_1.place(
-    x=611.0,
-    y=206.0,
-    width=33.0,
-    height=38.0
+    x=450.0,
+    y=360.0,
+    width=104.0,
+    height=37.0
 )
 
 button_image_2 = PhotoImage(
@@ -229,7 +213,7 @@ button_2 = Button(
     image=button_image_2,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: print("button_2 clicked"),
+    command=lambda: openFile("logs"),
     relief="flat"
 )
 button_2.place(
@@ -245,40 +229,38 @@ button_3 = Button(
     image=button_image_3,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: save(),
+    command=lambda: openFile("data"),
     relief="flat"
 )
 button_3.place(
-    x=450.0,
-    y=360.0,
-    width=104.0,
-    height=37.0
+    x=611.0,
+    y=206.0,
+    width=33.0,
+    height=38.0
 )
 
-canvas.create_rectangle(
-    0.0,
-    66.0,
-    336.0,
-    423.0,
-    fill="#404040",
-    outline="")
-
-canvas.create_text(
-    11.0,
-    77.0,
-    anchor="nw",
-    text="INSTRUCTION:\nName your image classification model.\nChoose a path in which you store your data.\nThis should be a folder, with different sub-folders\n for different classes.\nIn each sub-folder should be all of your data\n(in this case they are all the images\nto be used to train the model).\nChoose a path to store the logs when training your data.\nAfter you have entered everything,\npress OK to move to next step.",
-    fill="#FFFFFF",
-    font=("Inter", 12 * -1)
+image_image_5 = PhotoImage(
+    file=relative_to_assets("image_5.png"))
+image_5 = canvas.create_image(
+    168.0,
+    244.0,
+    image=image_image_5
 )
 
-canvas.create_text(
-    11.0,
-    296.0,
-    anchor="nw",
-    text="Made by David Pham\nContact information:\nGithub\nLinkedin\nDiscord: eddee296\nEmail: dav1dph869@gmail.com",
-    fill="#FFFFFF",
-    font=("Inter", 12 * -1)
+image_image_6 = PhotoImage(
+    file=relative_to_assets("image_6.png"))
+image_6 = canvas.create_image(
+    163.0,
+    346.0,
+    image=image_image_6
+)
+
+image_image_7 = PhotoImage(
+    file=relative_to_assets("image_7.png"))
+image_7 = canvas.create_image(
+    163.0,
+    159.0,
+    image=image_image_7
 )
 window.resizable(False, False)
 window.mainloop()
